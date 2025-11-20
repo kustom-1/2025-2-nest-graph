5f7f12cc20f2429a8554f68b8c97e023b52442d7
RaulQode <raulqode@gmail.com>
2025-11-20 12:08:37 -0500

    deleting REST swagger and pipe to validate input types on graph

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
-import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix("api")
+  
+  // ValidationPipe para validar los InputTypes de GraphQL
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: true
+      forbidNonWhitelisted: false,
+      transform: true,
     })
   )
 
-  const config = new DocumentBuilder()
-  .setTitle("Students Restful API")
-  .setDescription("Student management backend")
-  .setVersion("1.0")
-  .addBearerAuth(
-    {
-      type: "http",
-      scheme: "bearer",
-      bearerFormat: "JWT",
-      name:"JWT",
-      description: "Enter jwt token",
-      in: "header"
-    },
-    'JWT-auth'
-  )
-  .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
-
-
   await app.listen(process.env.PORT ?? 3000);
+  console.log(`🚀 GraphQL API running on: http://localhost:${process.env.PORT ?? 3000}/graphql`);
 }
 bootstrap();

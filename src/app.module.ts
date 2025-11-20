83ca155a0f60727d9151a2fde48b18a6ca7c787d
RaulQode <raulqode@gmail.com>
2025-11-20 12:08:05 -0500

    update app module to use graphql and database env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,32 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { StudentsModule } from './students/students.module';
 import { SeedModule } from './seed/seed.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: true,
+      sortSchema: true,
+      context: ({ req }) => ({ req }),
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
       port: 5432,
-      database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
+      username: process.env.DB_USERNAME || 'postgres',
+      database: process.env.DB_NAME || 'your_database',
+      password: process.env.DB_PASSWORD || 'your_password',
       autoLoadEntities: true,
       synchronize: true //Solo usarla en ambientes bajos, en prod hacer migraciones
     }),
